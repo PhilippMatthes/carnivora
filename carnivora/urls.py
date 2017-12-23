1361af0d300275b01d03b7004d7d04b49a9243cf
PhilippMatthes <github@philippmatth.es>
2017-12-23 11:34:20 +0100

    Base Structure

--- carnivora/urls.py
+++ carnivora/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from lure.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
 ]
--- lure/views.py
+++ lure/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+
 
 # Create your views here.
+
+
+def index(request):
+    return render_to_response('index.html', {'variable': 'world'})
