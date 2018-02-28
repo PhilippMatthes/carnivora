b3f623bcfe4c241480d39045e3e3078980f7e35b
PhilippMatthes <github@philippmatth.es>
2018-02-28 18:25:16 +0100

    Minor changes

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
+    print(sys.path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carnivora.settings")
     try:
         from django.core.management import execute_from_command_line
