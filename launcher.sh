8b450351d08216cb78692c4510e88b16cb8c72fe
Philipp Matthes <github@philippmatth.es>
2018-03-02 12:32:18 +0100

    Update launcher file for virtual environment on raspberry pi

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
@@ -6,5 +6,6 @@ cd /
 cd home/pi/Desktop/carnivora
 sudo route add default gw 192.168.178.1
 sudo git pull
-sudo python3 manage.py runserver 192.168.178.30:8000 --noreload
+source /home/pi/tensorflow/bin/activate
+sudo /home/pi/tensorflow/bin/python3 manage.py runserver 192.168.178.30:8000 --noreload
 cd /
