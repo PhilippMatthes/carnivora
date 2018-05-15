b74122f89dc6f7d714825a0888d4122f38b3e8f0
PhilippMatthes <github@philippmatth.es>
2018-05-15 11:30:59 +0200

    Change Launcher

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,7 @@ y#!/bin/bash
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/Desktop/carnivora
-sudo route add default gw 192.168.178.1
+cd home/pi/carnivora
 sudo git pull
 source /home/pi/tensorflow/bin/activate
 sudo /home/pi/tensorflow/bin/python3 manage.py runserver 192.168.178.30:8000
